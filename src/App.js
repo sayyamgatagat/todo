8390ed12dbd60f0b1d2d01f2fdacc8a3c8f28578
Sayyam Gatagat <Sayyamg24@gmail.com>
2020-10-05 22:48:23 +0530

    started with todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
-import login from './login';
 
 function App() {
   return(
-      <login />
+    
+  <div className='App'>    
+  <a href='#' className='right logout'>Logout</a>
+  <h1>Todo List</h1>  
+      <div className='todo_input'>
+      <div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="What's on your mind?" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+  <div class="input-group-append">
+    <button id="button-addon2" class="btn greenbutton" type="button" >+</button>
+  </div>
+</div>
+    </div>
+    </div> 
     );
     
 }
